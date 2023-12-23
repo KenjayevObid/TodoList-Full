eb9444042fdcd23b678e438b6d490a82db5030f3
KenjayevObid <114725656+KenjayevObid@users.noreply.github.com>
2023-12-24 00:07:44 +0500

    todolist

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://todo:gf3nD5sbqGRMEdB6@cluster0.ouyes8w.mongodb.net/?retryWrites=true&w=majority';
+
+  // export const CONNECTION_URL_DECKTOP = "mongodb://localhost:27017/todo"
+
+
+
